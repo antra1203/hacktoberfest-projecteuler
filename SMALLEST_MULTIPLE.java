8d2a8c2a73f36a5ea0b4cb06655a266c1b9607c7
antra1203 <34623425+antra1203@users.noreply.github.com>
2018-10-31 23:01:11 +0530

    Create SMALLEST_MULTIPLE.java

---
+++ SMALLEST_MULTIPLE.java
@@ -0,0 +1,23 @@
+static long FindLcm(long a,long b)
+ {
+     long lcm,hcf = 0;
+     long i=1;
+     long ger=a>b?a:b;
+     while(i<ger)
+     {
+         if((a%i==0) && (b%i==0))
+             hcf=i;
+         i++;
+     }
+     lcm=(a*b)/hcf;
+     return lcm;
+ }
+ static void FindMultiple()
+ {
+     long lcm=1;
+     for(long i=2;i<=20;i++)
+     {
+         lcm=FindLcm(lcm,i);
+     }   
+     System.out.println("Lcm="+lcm);
+ }
